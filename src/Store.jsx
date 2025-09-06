fee75b32a709d46987c6bf61dd0322cb9e1786d2
omidsdgi <omid69sdgi@gmail.com>
2025-09-06 22:55:08 +0330

    build the cart overview with Redux Selector

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/user/userSlice.js';
+import cartSlice from './features/cart/CartSlice.jsx';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    cart: cartSlice,
   },
 });
 export default store;
