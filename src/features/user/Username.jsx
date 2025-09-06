e2ce5100c1e06a3b8570eb461fec2cc5a7ec7a00
omidsdgi <omid69sdgi@gmail.com>
2025-09-06 14:15:58 +0330

    model user state with Redux Toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden font-bold md:block">Mehraneh</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return <div className="hidden font-bold md:block">{username}</div>;
 }
 
 export default Username;
