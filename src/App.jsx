b8cce87b1fe933312044dc914394f6e1a499fffc
omidsdgi <omid69sdgi@gmail.com>
2025-11-23 20:21:03 +0330

    feat(router): implement home route using createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
 import Home from './ui/Home.jsx';
-import Menu, { loader as menuLoader } from './features/menu/Menu.jsx';
-import Cart from './features/cart/Cart.jsx';
-import CreateOrder, {
-  action as createOrderAction,
-} from './features/order/CreateOrder.jsx';
-import Order, { loader as orderLoader } from './features/order/Order.jsx';
-import { action as updateOrderAction } from './features/order/UpdateOrder.jsx';
+import Menu from './features/menu/Menu.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { AppLayout } from './ui/AppLayout.jsx';
-import Error from './ui/Error.jsx';
+import Cart from './features/cart/CartSlice.jsx';
+import Order from './features/order/Order.jsx';
+import CreateOrder from './features/order/CreateOrder.jsx';
 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      { path: '/', element: <Home /> },
-      {
-        path: '/menu',
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      { path: '/cart', element: <Cart /> },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrderAction,
-      },
-    ],
+
+const router= createBrowserRouter([
+  {path:'/',
+  element: <Home />
   },
-]);
+  {
+    path:'/menu',
+    element: <Menu />,
+  },
+  {
+  path:'/cart',
+    element: <Cart/>,
+  },
+  {
+    path:'/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path:'/order/:orderId',
+    element: <Order/>,
+  }
+])
+
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>
 }
 export default App;
