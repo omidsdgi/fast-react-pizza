2cac420695b0255225a3623c135f0558cd877b75
omidsdgi <omid69sdgi@gmail.com>
2025-11-24 21:18:23 +0330

    feat(menu): style component using Tailwind CSS

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,19 +5,18 @@ import MenuItem from './MenuItem.jsx';
 
 function Menu() {
   const menu= useLoaderData()
-  console.log("menu",menu);
   return (
-    <ul>
-    {menu.map((pizza)=> (
-      <MenuItem pizza={pizza} key={pizza.id}/>
-    ))}
-    </ul>
-  )
-}
+      <ul className="divide-y divide-stone-200 px-2">
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+      )
+      }
 
-export async function loader() {
-  const menu = await getMenu()
-  return menu
-}
+      export async function loader() {
+      const menu = await getMenu()
+      return menu
+    }
 
-export default Menu;
+      export default Menu;
