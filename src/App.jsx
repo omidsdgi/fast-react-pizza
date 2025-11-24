6f3441c6c6760e4da51334c6eb8832b5ade1de19
omidsdgi <omid69sdgi@gmail.com>
2025-11-24 13:06:35 +0330

    feat(order): fetch orders data

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Home from './ui/Home.jsx';
 import Menu, {loader as menuLoader} from './features/menu/Menu.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Cart from './features/cart/Cart.jsx';
-import Order from './features/order/Order.jsx';
+import Order, {loader as orderLoader} from './features/order/Order.jsx';
 import CreateOrder from './features/order/CreateOrder.jsx';
 import AppLayout from './ui/AppLayout.jsx';
 import Error from './ui/Error.jsx';
+import loader from './ui/Loader.jsx';
 
 const router= createBrowserRouter([
   {
@@ -24,7 +25,12 @@ const router= createBrowserRouter([
       },
       {path:'/cart', element: <Cart/>},
       {path:'/order/new', element: <CreateOrder />},
-      {path:'/order/:orderId', element: <Order/>}
+      {
+        path:'/order/:orderId',
+        element: <Order/>,
+        loader: orderLoader,
+        errorElement:<Error/>,
+      }
     ]
   }
 ])
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,28 +1,17 @@
 // Test ID: IIDSAT
-import { useFetcher, useLoaderData } from 'react-router-dom';
 
-import OrderItem from './OrderItem';
-
-import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from '../../utils/helpers';
-import { useEffect } from 'react';
-import UpdateOrder from './UpdateOrder';
+} from "../../utils/helpers";
+import { getOrder } from '../../services/apiRestaurant.js';
+import { useLoaderData } from 'react-router-dom';
+
 
 function Order() {
-  const order = useLoaderData();
-  const fetcher = useFetcher();
-
-  useEffect(
-    function () {
-      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
-    },
-    [fetcher]
-  );
-
+  const order= useLoaderData();
+  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
     status,
@@ -32,73 +21,39 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
-
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div className="space-y-8 px-4 py-6">
-      <div className="flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-xl font-semibold">Order #{id} status</h2>
+    <div>
+      <div>
+        <h2>Status</h2>
 
-        <div className="space-x-2">
-          {priority && (
-            <span className="rounded-full bg-red-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-50">
-              Priority
-            </span>
-          )}
-          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
-            {status} order
-          </span>
+        <div>
+          {priority && <span>Priority</span>}
+          <span>{status} order</span>
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
-        <p className="font-medium">
+      <div>
+        <p>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : 'Order should have arrived'}
+            : "Order should have arrived"}
         </p>
-        <p className="text-xs text-stone-500">
-          (Estimated delivery: {formatDate(estimatedDelivery)})
-        </p>
+        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
 
-      <ul className="dive-stone-200 divide-y border-b border-t">
-        {cart.map((item) => (
-          <OrderItem
-            item={item}
-            key={item.pizzaId}
-            isLoadingIngredients={fetcher.state === 'loading'}
-            ingredients={
-              fetcher?.data?.find((el) => el.id === item.pizzaId)
-                ?.ingredients ?? []
-            }
-          />
-        ))}
-      </ul>
-
-      <div className="space-y-2 bg-stone-200 px-6 py-5">
-        <p className="text-sm font-medium text-stone-600">
-          Price pizza: {formatCurrency(orderPrice)}
-        </p>
-        {priority && (
-          <p className="text-sm font-medium text-stone-600">
-            Price priority: {formatCurrency(priorityPrice)}
-          </p>
-        )}
-        <p className="font-bold">
-          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
-        </p>
+      <div>
+        <p>Price pizza: {formatCurrency(orderPrice)}</p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
-
-      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
 
-export async function loader({ params }) {
-  const order = await getOrder(params.orderId);
-  return order;
+export async function loader({params}){
+    const loader= await getOrder(params.orderId)
+  return loader;
 }
-
 export default Order;
