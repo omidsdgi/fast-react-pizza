15f6402ed94f28a0a31b5ac44c685781c5f06720
omidsdgi <omid69sdgi@gmail.com>
2025-11-27 20:17:41 +0330

    feat(creat-order): add username using useSelector

--- src/features/cart/CartSlice.jsx
+++ src/features/cart/CartSlice.jsx
@@ -1,7 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cart: [],
+  // cart: [],
+cart:[
+  {
+    pizzaId:12,
+  name:'Med',
+    quantity:2,
+    unitPrice:16,
+    totalPrice:42,
+  },
+]
 };
 
 const cartSlice = createSlice({
@@ -12,45 +21,21 @@ const cartSlice = createSlice({
       state.cart.push(action.payload);
     },
     deleteItem(state, action) {
-      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+      state.cart=state.cart.filter(item=>item.pizzaId !== action.payload);
     },
     increaseItemQuantity(state, action) {
-      const item = state.cart.find((item) => item.pizzaId === action.payload);
-
-      item.quantity++;
-      item.totalPrice = item.quantity * item.unitPrice;
+  const item = state.cart.find((item => item.pizzaId === action.payload))
+      item.quantity ++
+      item.totalPrice= item.quantity * item.unitPrice
     },
     decreaseItemQuantity(state, action) {
-      const item = state.cart.find((item) => item.pizzaId === action.payload);
-
-      item.quantity--;
-      item.totalPrice = item.quantity * item.unitPrice;
-
-      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+      const item= state.cart.find(item=>item.pizzaId === action.payload)
+      item.quantity --
+      item.totalPrice= item.quantity * item.unitPrice
     },
-    clearCart(state) {
-      state.cart = [];
-    },
-  },
-});
+    clearItem(state) {
+      state.cart= []
+    }
+  }
+})
 
-export const {
-  addItem,
-  deleteItem,
-  increaseItemQuantity,
-  decreaseItemQuantity,
-  clearCart,
-} = cartSlice.actions;
-
-export default cartSlice.reducer;
-
-export const getCart = (state) => state.cart.cart;
-
-export const getTotalCartQuantity = (state) =>
-  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
-
-export const getTotalCartPrice = (state) =>
-  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
-
-export const getCurrentQuantityById = (id) => (state) =>
-  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
