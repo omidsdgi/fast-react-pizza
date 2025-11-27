8ed300da5820890b4d4a2278f51ea03b8c6b32f0
omidsdgi <omid69sdgi@gmail.com>
2025-11-27 23:45:23 +0330

    feat(cat-overwiev): add useSelector to get total quantity and price

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,29 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
-import Button from '../../ui/Button';
-import DeleteItem from '../cart/DeleteItem';
-import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 import { formatCurrency } from '../../utils/helpers';
-import { addItem, getCurrentQuantityById } from '../cart/CartSlice.jsx';
+import Button from '../../ui/Button.jsx';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../cart/CartSlice.jsx';
 
 function MenuItem({ pizza }) {
-  const dispatch = useDispatch();
+const dispatch = useDispatch();
+  const {id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
-
-  const currentQuantity = useSelector(getCurrentQuantityById(id));
-  const isInCart = currentQuantity > 0;
-
-  function handleAddToCart() {
-    const newItem = {
-      pizzaId: id,
-      name,
-      quantity: 1,
-      unitPrice,
-      totalPrice: unitPrice * 1,
-    };
-    dispatch(addItem(newItem));
+  function handleAddToCart(){
+const newItem={
+  pizzaId:id,
+  name,
+  quantity:1,
+  unitPrice,
+  totalPrice:unitPrice *1,
+};
+dispatch(addItem(newItem));
   }
-
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -44,22 +37,7 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-
-          {isInCart && (
-            <div className="flex items-center gap-3 sm:gap-8">
-              <UpdateItemQuantity
-                pizzaId={id}
-                currentQuantity={currentQuantity}
-              />
-              <DeleteItem pizzaId={id} />
-            </div>
-          )}
-
-          {!soldOut && !isInCart && (
-            <Button type="small" onClick={handleAddToCart}>
-              Add to cart
-            </Button>
-          )}
+          {!soldOut && <Button type='small' onClick={handleAddToCart} >Add to cart</Button>}
         </div>
       </div>
     </li>
