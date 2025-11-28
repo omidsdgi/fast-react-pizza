142aae78c6f0c9f3c7d170eb42c2517dddf84dd6
omidsdgi <omid69sdgi@gmail.com>
2025-11-28 22:12:07 +0330

    fix: change export to export defualt

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,15 +2,16 @@ import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder.jsx';
 import Username from '../features/user/Username.jsx';
 
-export function Header() {
+function Header() {
   return (
-    <header className="font-pizza flex items-center justify-between border-b border-stone-400 bg-yellow-600 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.{' '}
+        Fast React Pizza Co.
       </Link>
       <SearchOrder />
-
       <Username />
     </header>
   );
 }
+
+export default Header;
