bd95e73396d09b48ca5c96d20c981bf60be24a2e
omidsdgi <omid69sdgi@gmail.com>
2025-11-28 21:40:59 +0330

    fix(link-button): rename classname

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 function LinkButton({ children, to }) {
   const navigate = useNavigate();
   const classname = 'text-sm text-blue-500 hover:text-blue-600 hover:underline';
-  if (!to)
+  if (to === '-1')
     return (
       <button className={classname} onClick={() => navigate(-1)}>
         {children}
