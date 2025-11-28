72886f9ab7f68b76734b8bcb1aa6444523aac37a
omidsdgi <omid69sdgi@gmail.com>
2025-11-28 12:29:12 +0330

    feat(menu-item): remove item automatically when quantity reaches zero

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,21 +1,25 @@
 import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button.jsx';
-import { useDispatch } from 'react-redux';
-import { addItem } from '../cart/CartSlice.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, getCurrentQuantityById } from '../cart/CartSlice.jsx';
+import DeleteItem from '../cart/DeleteItem.jsx';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity.jsx';
 
 function MenuItem({ pizza }) {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const {id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity=useSelector(getCurrentQuantityById(id))
+  const isInCart= currentQuantity>0;
 
   function handleAddToCart(){
-const newItem={
-  pizzaId:id,
-  name,
-  quantity:1,
-  unitPrice,
-  totalPrice:unitPrice *1,
-};
-dispatch(addItem(newItem));
+    const newItem={
+      pizzaId:id,
+      name,
+      quantity:1,
+      unitPrice,
+      totalPrice:unitPrice *1,
+    };
+    dispatch(addItem(newItem));
   }
   return (
     <li className="flex gap-4 py-2">
@@ -37,7 +41,14 @@ dispatch(addItem(newItem));
               Sold out
             </p>
           )}
-          {!soldOut && <Button type='small' onClick={handleAddToCart} >Add to cart</Button>}
+
+          {isInCart && (
+            <div className='flex items-center justify-between gap-3 sm:gap-8'>
+              <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity} />
+            <DeleteItem pizzaId={id} />
+            </div>
+          )}
+          {!soldOut && !isInCart && <Button type='small' onClick={handleAddToCart} >Add to cart</Button>}
         </div>
       </div>
     </li>
