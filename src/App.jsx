d8022dc66e623e41d94df20c8b426f67b03b52ad
omidsdgi <omid69sdgi@gmail.com>
2025-11-28 21:51:01 +0330

    feat(App): add updateOrderAction

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Order, {loader as orderLoader} from './features/order/Order.jsx';
 import CreateOrder,{action as createOrderAction} from './features/order/CreateOrder.jsx';
 import AppLayout from './ui/AppLayout.jsx';
 import Error from './ui/Error.jsx';
-import loader from './ui/Loader.jsx';
+import  {action as updateOrderAction } from './features/order/UpdateOrder.jsx';
 
 const router= createBrowserRouter([
   {
@@ -37,6 +37,7 @@ const router= createBrowserRouter([
         element: <Order/>,
         loader: orderLoader,
         errorElement:<Error/>,
+        action: updateOrderAction,
       }
     ]
   }
